c661b21da8668912df6e8ab19dd02a0727c799b2
godori <gelbbo93@gmail.com>
2023-07-27 16:15:58 +0900

    #42 - id 타입 변경
    long에서 String으로 타입변경

--- src/main/java/com/board/board/dto/ArticleWithCommentsDto.java
+++ src/main/java/com/board/board/dto/ArticleWithCommentsDto.java
@@ -7,7 +7,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public record ArticleWithCommentsDto(
-    Long id,
+    String id,
     UserAccountDto userAccountDto,
     Set<ArticleCommentDto> articleCommentDtos,
     String title,
@@ -18,7 +18,7 @@ public record ArticleWithCommentsDto(
     LocalDateTime modifiedAt,
     String modifiedBy
 ) {
-  public static ArticleWithCommentsDto of(Long id, UserAccountDto userAccountDto, Set<ArticleCommentDto> articleCommentDtos, String title, String content, String hashtag, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+  public static ArticleWithCommentsDto of(String id, UserAccountDto userAccountDto, Set<ArticleCommentDto> articleCommentDtos, String title, String content, String hashtag, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
     return new ArticleWithCommentsDto(id, userAccountDto, articleCommentDtos, title, content, hashtag, createdAt, createdBy, modifiedAt, modifiedBy);
   }
 
